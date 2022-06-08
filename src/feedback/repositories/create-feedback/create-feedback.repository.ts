f35ab586c8f33e37209c4ec81178fc4498f11e34
jvricarte <victor_rs11@hotmail.com>
2022-06-07 21:41:50 -0300

    refact: inject prisma cliente dependency to repository

--- src/feedback/repositories/create-feedback/create-feedback.repository.ts
+++ src/feedback/repositories/create-feedback/create-feedback.repository.ts
@@ -1,10 +1,17 @@
-import { prisma } from "../../../prisma";
+import { PrismaClient } from "@prisma/client";
 import { Feedback } from "../../domain/feedback.entity";
 import { CreateFeedbackRepositoryContract } from "./create-feedback.repository.contract";
 
+
 export class CreateFeedbackRepository implements CreateFeedbackRepositoryContract {
+  private prismaClient: PrismaClient;
+
+  constructor(prismaClient: PrismaClient) {
+    this.prismaClient = prismaClient;
+  }
+
   async create(data: Feedback): Promise<Feedback> {
-    return await prisma.feedBack.create({
+    return await this.prismaClient.feedBack.create({
       data,
     });
   }
--- src/routes.ts
+++ src/routes.ts
@@ -2,8 +2,9 @@ import { Router } from "express";
 import { FeedbackController } from "./feedback/feedback.controller";
 import { CreateFeedbackUseCase } from "./feedback/use-cases/create-feedback/create-feedback.use-case";
 import { CreateFeedbackRepository } from "./feedback/repositories/create-feedback/create-feedback.repository";
+import { prisma } from "./prisma";
 
-const repository = new CreateFeedbackRepository();
+const repository = new CreateFeedbackRepository(prisma);
 const useCase = new CreateFeedbackUseCase(repository);
 
 const routes = Router();
